9d68c4e1a7dc8c1b8a08f55778d38799287a271d
etomsky <etomsy@yahoo.com>
2018-09-07 16:01:54 -0400

    Moving Basket with Mouse and Catching Apples
    Moving Baskets with the Mouse, Catching Apples, and duplicating scene to create variant with higher difficulty settings

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -11,6 +11,28 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // The camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Find out what hit this basket
+        GameObject collidedWith = collision.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
 }
